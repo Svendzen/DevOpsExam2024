9e9b0a7f5d608cc7ea930dfb951fd798e5a0dc29
EC2 Default User <ec2-user@ip-172-31-45-173.eu-west-1.compute.internal>
2024-11-12 20:23:52 +0000

    Provider requires versions or higher. Added code for .tfstate setup

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,3 +1,22 @@
+# providers.tf
+
+terraform {
+  required_version = ">= 1.9.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.74.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "pgr301-2024-terraform-state"  # Bucket for lagring av Terraform state
+    key    = "infra/terraform.tfstate"       # Filsti for state-filen i bucketen
+    region = "eu-west-1"                     # Region for S3-bucketen
+  }
+}
+
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-west-1"  # Region hvor ressursene skal opprettes
 }
